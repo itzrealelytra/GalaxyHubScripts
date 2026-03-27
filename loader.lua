6ae1df1fb919fe4ce8a374b57d712e8f0948ed70
itzrealelytra <itzrealelytra@gmail.com>
2026-03-27 16:26:38 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,17 +1,15 @@
-local HttpService = game:GetService("HttpService")
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a68ed82115534fb6ea10431a7fde53cf.lua"))()
+end)
 
-local scripts = {
-    "https://api.luarmor.net/files/v4/loaders/a68ed82115534fb6ea10431a7fde53cf.lua",
-    "https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua",
-    "https://api.luarmor.net/files/v4/loaders/64b255d858bf16bc967c86f0df12b78b.lua"
-}
+task.wait(1)
 
-for i, url in ipairs(scripts) do
-    local success, result = pcall(function()
-        loadstring(game:HttpGet(url))()
-    end)
-    if not success then
-        warn("Script "..i.." failed: "..result)
-    end
-    task.wait(1)
-end
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"))()
+end)
+
+task.wait(1)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/64b255d858bf16bc967c86f0df12b78b.lua"))()
+end)
